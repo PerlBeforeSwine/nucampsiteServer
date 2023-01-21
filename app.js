b046451476336d5408e884ef8fff1edaba8f19f2
Ela Gavrilova <egavrilova@indeed.com>
2023-01-21 13:06:18 -0800

    updated to use mongodb

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const promotionRouter = require('./routes/promotionRouter');
 
+const mongoose = require('mongoose');
+
+const url = 'mongodb://127.0.0.1:27017/nucampsite';
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connect.then(
+  () => console.log('Connected correctly to server'),
+  (err) => console.log(err)
+);
+
 var app = express();
 
 // view engine setup
@@ -30,12 +45,12 @@ app.use('/promotions', promotionRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
